692ecf1725d7b03e75cca22c57f359a0f6b23741
Connor Shearwood <drake@lifein2d.co.uk>
2011-10-08 14:44:33 +0100

    Added some raw data

--- system/healthq.class.php
+++ system/healthq.class.php
@@ -5,7 +5,6 @@
 		public function __construct($routine, $data, $settings) {
 			$this->settings = parse_ini_file($settings, true);
 			//$this->db = $this->connectToDatabase();
-			$this->generateReplaceableValues();
 			//$this->loggedin = false;
 			//$this->user = 0;
 			$this->timeStarted = microtime(true);
@@ -30,16 +29,6 @@
 			$pagedata["body"] = "<p>This is a working test page.</p>";
 			$this->display(false, $pagedata);
 		}
-
-		private function uuid() {
-			return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-				mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
-				mt_rand( 0, 0xffff ),
-				mt_rand( 0, 0x0fff ) | 0x4000,
-				mt_rand( 0, 0x3fff ) | 0x8000,
-				mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
-			);
-		}
 		
 		private function error($errortype) {
 			switch ($errortype) {
@@ -55,21 +44,6 @@
 					$this->display(true, "404");
 					break;
 			}
-		}
-		
-		private function generateReplaceableValues() {
-			if (!$this->db) {
-				$this->display(true, "index", "There has been an error with the database. Please try again later.");
-			}
-			$this->settings['replace']['<siteurl>'] = "http://" . $_SERVER["HTTP_HOST"];
-			if ($this->isLoggedIn()) {
-				$result = $this->db->query("SELECT name FROM users WHERE id='" . $this->user . "';");
-				$result = $result->fetch_assoc();
-				$this->settings['replace']['<username>'] = $result['name'];
-			} else {
-				$this->settings['replace']['<username>'] = 'guest';
-			}
-			$this->settings['replace']['`'] = '\'';
 		}
 		
 		private function display($template, $data, $error=NULL) {
