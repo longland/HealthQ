31f92afcbf550589a8a251cc7820d94269fc00b7
Chirag Desai <djchillerz@gmail.com>
2011-10-08 21:20:40 +0100

    added facebook login

---
+++ system/login.php
@@ -0,0 +1,13 @@
+$data["body"] = "
+
+<div id=\"fb-root\"></div>
+<script src=\"http://connect.facebook.net/en_US/all.js\"></script>
+<script>
+FB.init({ 
+        appId:'175392325877131', cookie:true, 
+        status:true, xfbml:true 
+        });
+</script>
+<fb:login-button>Login with Facebook</fb:login-button>
+
+";
