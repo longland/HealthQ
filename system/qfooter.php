69d0e359d2f6452873fab2a41ec2db6370837670
Chirag Desai <djchillerz@gmail.com>
2011-10-09 13:13:14 +0100

    updated question header and footer with qheader and qfooter

---
+++ system/qfooter.php
@@ -0,0 +1,13 @@
+</section>
+      <footer>
+  
+  <nav class="round">
+   <br />
+   <h2>Maybe your friends can help? Ask them on Facebbok</h2>
+    <a href="javascript:FB.ui({ method: 'feed', 
+            message: 'Facebook for Websites is super-cool'})" class="tell_button round">Tell your friends.</a>
+    <br />
+  </nav>
+<?php    include 'footer.php'; ?>
+</body>
+</html>
