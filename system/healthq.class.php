ee1591bbec2d9ad5b80f0c970387934a86441220
Connor Shearwood <drake@lifein2d.co.uk>
2011-10-09 06:58:13 +0100

    Added more crime functionality and the ability to answer questions. Still needs update statements.

--- system/healthq.class.php
+++ system/healthq.class.php
@@ -48,6 +48,8 @@
 			
 			$crimeresult = $this->db->query("SELECT USER_ID, STATUS_ID, TYPE_ID, LAT, LONG, USER_ID WHERE ID = " . (int)$argument[0] . " LIMIT 1;");
 			$crimeresult = $crimeresult->fetch_assoc();
+			$statusresult = $this->db->query("SELECT TYPE_NAME FROM CRIME_TYPE WHERE TYPE_ID = " . (int)$crimeresult["TYPE_ID"] . " LIMIT 1;";
+			$statusresult = $statusresult->fetch_assoc();
 			$profile_id = $_COOKIE["fbs_175392325877131"];
 			$profile_id = explode("&",$profile_id);
 			$profile_id = explode("=",$profile_id[6]);
@@ -57,12 +59,13 @@
 			$thequestion = $this->db->query("SELECT QUESTION, OPTION1, OPTION2, OPTION3, OPTION4, ANSWER FROM QUESTIONS WHERE ID = " . (int)$questionresult[""] . " LIMIT 1;");
 			$thequestion = $thequestion->fetch_assoc();
 			
-			$crime = "There has been a kidnapping! The kidnapper left a note, solve the riddle to solve the crime:";
-			$question = "Where did Sherlock Holmes live?";
-			$answer[0] = "221b Baker Street";
-			$answer[1] = "92 Picadilly Road";
-			$answer[2] = "314d Derby Lane";
-			$correct = 0;
+			$crime = "A " . $statusresult["TYPE_NAME"] . " has occurred! Solve the riddle to solve the crime:";
+			$question = $thequestion["QUESTION"];
+			$answer[0] = $thequestion["OPTION1"];
+			$answer[1] = $thequestion["OPTION2"];
+			$answer[2] = $thequestion["OPTION3"];
+			$answer[3] = $thequestion["OPTION4"];
+			$correct = $thequestion["ANSWER"]-1;
 			
 			$data["head"]  = '<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# crimesapp: http://ogp.me/ns/fb/crimesapp#">';
 			$data["head"] .= '<meta property="fb:app_id"          content="175392325877131">';
@@ -73,11 +76,37 @@
 			$data["head"] .= '<meta property="og:image"           content="https://s-static.ak.fbcdn.net/images/devsite/attachment_blank.png"> ';
 			$data["head"] .= '<meta property="crimesapp:question" content="' . $crime . " " . $question . '"> ';
 			$data["head"] .= '<meta property="crimesapp:answer"   content="' . $answer[$correct] . '"> ';
-			$data["title"] = "There Has Been A Crime!";
-			$data["body"]  = "<p>There has been a crime!<br />" . $crime . "<br />" . $question . "<br /><br />";
-			$data["body"] .= "<a onclick=" . "</p>";
+			
+			require_once "header.php";
+			echo "<p>There has been a crime!<br />" . $crime . "<br />" . $question . "<br /><br />";
+			for ($i = 0; $i > 3; $i++) {
+				echo "<a href='http://health.itza.uk.com/answer/" . (int)$argument[0] . "/" . $i . "' href='#'>" . $answer[$i] . "</a><br />";
+			}
+			echo "</p>";
 			require_once "footer.php";
 			die();
+		}
+		
+		public function answer($args) {
+			$crimeresult = $this->db->query("SELECT USER_ID, STATUS_ID, TYPE_ID, LAT, LONG, USER_ID WHERE ID = " . (int)$argument[0] . " LIMIT 1;");
+			$crimeresult = $crimeresult->fetch_assoc();
+			$statusresult = $this->db->query("SELECT TYPE_NAME FROM CRIME_TYPE WHERE TYPE_ID = " . (int)$crimeresult["TYPE_ID"] . " LIMIT 1;";
+			$statusresult = $statusresult->fetch_assoc();
+			$profile_id = $_COOKIE["fbs_175392325877131"];
+			$profile_id = explode("&",$profile_id);
+			$profile_id = explode("=",$profile_id[6]);
+			$profile_id = $profile_id[1];
+			$questionresult = $this->db->query("SELECT USER_ID, Q_ID FROM USER WHERE PROFILE_ID = " . $profile_id . " LIMIT 1;");
+			$questionresult = $questionresult->fetch_assoc();
+			$thequestion = $this->db->query("SELECT ANSWER FROM QUESTIONS WHERE ID = " . (int)$questionresult[""] . " LIMIT 1;");
+			$thequestion = $thequestion->fetch_assoc();
+			if ($args[1] == $thequestion["ANSWER"]) {
+				// Update to Solved
+				header("Location: http://health.itza.uk.com/welldone/");
+			} else {
+				// Update to Unsolved, remove user
+				header("Location: http://health.itza.uk.com/muppet/");
+			}
 		}
 		
 		public function saveuser($args) {
